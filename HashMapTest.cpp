df01a9f0e605a1bdb9ba21281c4cfc30a2785eee
aozturk <ozturkabdullah@gmail.com>
2013-12-07 21:39:21 +0200

    Make it generic with custom hash function

---
+++ HashMapTest.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include "HashMap.h"
+
+using namespace std;
+
+struct MyKeyHash {
+    unsigned long operator()(const int& k) const
+    {
+        return k % 10;
+    }
+};
+
+int main() 
+{
+	HashMap<int, string, MyKeyHash> hmap;
+	hmap.put(1, "1");
+	hmap.put(2, "2");
+	hmap.put(3, "3");
+ 
+	string value;
+	bool result = hmap.get(2, value);
+	assert(result);
+	assert(value == "2");
+
+	result = hmap.get(3, value);
+	assert(result);
+	assert(value == "3");
+
+	hmap.remove(3);
+	result = hmap.get(3, value);
+	assert(!result);
+
+	cout << "OK!" << endl;
+}
