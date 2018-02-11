826516bf32cadaa59f4c01b6d7c90f32895df11a
Abhijeet Kandalkar <kandalkar.abhijeet58@gmail.com>
2018-02-12 01:22:19 +0530

    Use static_cast instead of reinterpret_cast * static_cast is better choice for converting numeric data types * static_cast conversions are checked at compile time to prevent   obvious incompatibilities

--- hashmap/KeyHash.h
+++ hashmap/KeyHash.h
@@ -21,6 +21,6 @@ template <typename K, size_t tableSize>
 struct KeyHash {
     unsigned long operator()(const K &key) const
     {
-        return reinterpret_cast<unsigned long>(key) % tableSize;
+        return static_cast<unsigned long>(key) % tableSize;
     }
 };
